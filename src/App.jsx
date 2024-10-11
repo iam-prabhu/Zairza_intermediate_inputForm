b1ff979a09060161c52a9ceb7b8e6650498dc3c6
Prabhupratik Pattanaik <prabhupratikpattanaik@gmail.com>
2024-10-11 23:52:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import InputForm from './Components/InputForm';
+
+function App() {
+  
+  return (
+    <>
+      <InputForm/>
+      
+    </>
+  );
+}
+
+export default App
